bf004720375480ca247360560863003a83d73bbd
evgen122 <evg.khristenko@gmail.com>
2023-08-30 14:42:12 +0300

    finish with coment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import axios from 'axios';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+// import { infoBreedData } from '.';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_FEvTz5K5LqJQeCheyolHOvxNrx0foxzlFWzPJKK3QQcWirMU4Zh51kVFnC8wOO9U';
@@ -14,11 +15,62 @@ const elements = {
 };
 
 const urlBreeds = 'https://api.thecatapi.com/v1/breeds';
-// const urlImg = 'https://api.thecatapi.com/v1/images/search';
-
-elements.breedSellect.addEventListener('change', fetchCatByBreed);
+const urlImg = 'https://api.thecatapi.com/v1/images/search';
 
 elements.error.hidden = true;
+elements.loader.hidden = false;
+elements.breedSellect.hidden = true;
 
-fetchBreeds(urlBreeds);
-// fetchCatByBreed();
+elements.breedSellect.addEventListener('change', showBreed);
+
+fetchBreeds(urlBreeds)
+  .then(response => {
+    // console.log(response.data);
+    let markup = response.data
+      .map(breed => {
+        return `<option value='${breed.id}'>'${breed.name}'</option>`;
+      })
+      .join('');
+    elements.breedSellect.insertAdjacentHTML('beforeend', markup);
+    new SlimSelect({
+      select: '#selectElement',
+    });
+  })
+  .catch(error => {
+    elements.error.hidden = false; // handle error
+    // .log(error);
+  })
+  .finally(function () {
+    elements.breedSellect.hidden = false;
+    elements.loader.hidden = true; // always executed
+  });
+
+function showBreed(elm) {
+  elements.breedSellect.hidden = true;
+  elements.loader.hidden = false;
+  elements.error.hidden = true;
+  const breedId = elm.target.value;
+
+  fetchCatByBreed(breedId)
+    .then(response => {
+      elements.loader.hidden = true;
+      const infoBreed = `<img class="cat-pictures" src="${response.data[0].url}" alt="${response.data[0].id}"
+      width="50%">
+  <h2 class="cat-info-breed">${response.data[0].breeds[0].name}</h2>
+  <p class="cat-info-desc">${response.data[0].breeds[0].description}</p>
+
+  <p class="cat-info-temp"><b>Temperament:  </b>${response.data[0].breeds[0].temperament}</p>`;
+
+      elements.catInfo.innerHTML = infoBreed;
+      // elements.catInfo.insertAdjacentHTML('beforeend', infoBreed);
+    })
+
+    .catch(error => {
+      elements.catInfo.innerHTML = '';
+      elements.error.hidden = false; // handle error
+    })
+    .finally(function () {
+      elements.breedSellect.hidden = false;
+      elements.loader.hidden = true; // always executed
+    });
+}
